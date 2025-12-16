61c43f7955e48a3c765bc87c14c8b7980f5d87ae
MorganEdmonds <morganedmonds@gmail.com>
2025-12-16 15:55:03 -0500

    working on shopping cart DAO and shopping cart controller

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,16 @@
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
+
+    void addItem(ShoppingCartItem shoppingCartItem);
+
+    void updateItem(ShoppingCartItem shoppingCartItem);
+
+    void deleteAllItems(ShoppingCartItem shoppingCartItem);
     // add additional method signatures here
 }
---
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -0,0 +1,66 @@
+package org.yearup.data.mysql;
+
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+
+    public MySqlShoppingCartDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+
+
+        //
+        String sql = "SELECT * FROM shopping_cart WHERE user_id = ? ";
+        try(Connection connection = super.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
+            statement.setInt(1, userId);
+
+            try(ResultSet result = statement.executeQuery()){
+                if(result.next()){
+
+                    int userID = result.getInt(1);
+                    int productID = result.getInt(2);
+                    int quantity = result.getInt(3);
+
+//                    shoppingCart.containts(productID);
+
+                }
+            }
+
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    public void addItem(ShoppingCartItem shoppingCartItem) {
+
+    }
+
+    @Override
+    public void updateItem(ShoppingCartItem shoppingCartItem) {
+
+    }
+
+    @Override
+    public void deleteAllItems(ShoppingCartItem shoppingCartItem) {
+
+    }
+
+
+}
+
